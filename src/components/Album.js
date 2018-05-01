57adf2d55ae83ffb3bfa9b0b23f6839979c03105
Leah McNamee <leahmcnamee@ilcoud.com>
2018-04-30 21:21:45 -0700

    Components: Album assignment completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -12,7 +12,7 @@ class Album extends Component {
     this.state = {
       album: album
     };
-  }
+}
 
    render() {
      return (
@@ -32,6 +32,19 @@ class Album extends Component {
             <col id="song-duration -column" />
           </colgroup>
           <tbody>
+            {this.state.album.songs.map( (song, index) =>
+              <tr className="song" key={index} >
+                <td className="button">
+                <button>
+                  <span className="ion-play"></span>
+                  <span className="ion-pause"></span>
+                </button>
+                </td>
+                <td className="song-number">{index+1}</td>
+                <td className="song-title">{song.title}</td>
+                <td className="song-duration">{song.duration}</td>
+              </tr>
+            )}
           </tbody>
         </table>
        </section>
